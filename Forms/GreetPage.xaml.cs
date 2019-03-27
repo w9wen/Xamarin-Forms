35635e88e80f6f60ea3292d4147d716291d9c855
William Wen (Mac) <william.wen@tac.com.tw>
2019-03-28 06:26:51 +0800

    Show the slider value to label.

--- Forms/GreetPage.xaml.cs
+++ Forms/GreetPage.xaml.cs
@@ -8,13 +8,20 @@ namespace Forms
     public partial class GreetPage : ContentPage
     {
         public GreetPage()
-        {
+        { 
             InitializeComponent();
+            Slider_Temperature.Value = 0.5;
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
             DisplayAlert("title", "message", "cancel");
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            //Label_SliderValue.Text = e.NewValue.ToString();
+            Label_SliderValue.Text = String.Format("Value is {0:F2}", e.NewValue);
         }
     }
 }
