6ba7c3f3fc67c924517634a5b851b8da596e9428
William Wen (Mac) <william.wen@tac.com.tw>
2019-03-31 00:02:20 +0800

    3-26. Gird in Code, not work here.

--- Forms/GridPage.xaml.cs
+++ Forms/GridPage.xaml.cs
@@ -10,12 +10,33 @@ namespace Forms
         public GridPage()
         {
             InitializeComponent();
+
             var gird = new Grid 
             {
                 RowSpacing = 20,
                 ColumnSpacing = 40, 
             };
-            var label = new Label { };
+            var label = new Label {Text="Label 1" };
+            gird.Children.Add(label, 0, 0);
+            Grid.SetRowSpan(label, 2);
+            Grid.SetColumnSpan(label, 2);
+
+            Grid.SetRow(label, 0);
+            Grid.SetColumn(label, 0);
+
+            gird.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(100, GridUnitType.Absolute)
+            });
+
+            gird.RowDefinitions.Add(new RowDefinition 
+            {
+                Height = new GridLength(2, GridUnitType.Star) 
+            });
+            gird.RowDefinitions.Add(new RowDefinition
+            {
+                Height = new GridLength(1, GridUnitType.Auto)
+            });
         }
     }
 }
