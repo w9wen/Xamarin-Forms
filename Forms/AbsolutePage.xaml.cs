27ec172767cb15fad58e138d49f2af28b4cacf66
William Wen (Mac) <william.wen@tac.com.tw>
2019-04-02 05:40:31 +0800

    3-29. Absolute Layout in Code.

--- Forms/AbsolutePage.xaml.cs
+++ Forms/AbsolutePage.xaml.cs
@@ -10,6 +10,16 @@ namespace Forms
         public AbsolutePage()
         {
             InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+            Content = layout;
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(
+                aquaBox, 
+                new Rectangle(0, 0, 1, 1),
+                AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0.5, 0.5, 100, 100));
+            AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.PositionProportional);
         }
     }
 }
