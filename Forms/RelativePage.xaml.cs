91e178f520bfcac2ff703402348094c4d1e586e5
William Wen (Mac) <william.wen@tac.com.tw>
2019-04-03 06:29:45 +0800

    3-32. Relative Layout in XAML.

---
+++ Forms/RelativePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Forms
+{
+    public partial class RelativePage : ContentPage
+    {
+        public RelativePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
