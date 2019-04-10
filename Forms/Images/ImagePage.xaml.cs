8c318fc01be4a382ac0949aa4021b17e87710721
William Wen (Mac) <william.wen@tac.com.tw>
2019-04-11 05:16:29 +0800

    4-39. Embedded Images.

--- Forms/Images/ImagePage.xaml.cs
+++ Forms/Images/ImagePage.xaml.cs
@@ -10,15 +10,17 @@ namespace Forms.Images
         public ImagePage()
         {
             InitializeComponent();
-            var imageSrouce = (UriImageSource)ImageSource.FromUri(new Uri("http://lorempixel.com/1920/1080/sports/7/"));
-            //var imageSource = new UriImageSource { Uri = new Uri ("http://lorempixel.com/1920/1080/sports/7/") };
-            imageSrouce.CachingEnabled = false;
-            //imageSource.CacheValidity = TimeSpan.FromHours(10);
+            //var imageSrouce = (UriImageSource)ImageSource.FromUri(new Uri("http://lorempixel.com/1920/1080/sports/7/"));
+            ////var imageSource = new UriImageSource { Uri = new Uri ("http://lorempixel.com/1920/1080/sports/7/") };
+            //imageSrouce.CachingEnabled = false;
+            ////imageSource.CacheValidity = TimeSpan.FromHours(10);
 
-            Image_Sport.Source = imageSrouce;
-            //Image_Sport.Source = "http://lorempixel.com/1920/1080/sports/7/";
+            //Image_Sport.Source = imageSrouce;
+            ////Image_Sport.Source = "http://lorempixel.com/1920/1080/sports/7/";
 
-            //Image_Sport.Aspect = Aspect.AspectFill;
+            ////Image_Sport.Aspect = Aspect.AspectFill;
+
+            Image_Sport.Source = ImageSource.FromResource("Forms.Images.background.jpg");
         }
     }
 }
