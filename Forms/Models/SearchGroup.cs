1b8caf7cdce29d0a4c16d6f699b14daabcd0e11b
William Wen (NB) <william.wen@tac.com.tw>
2019-04-17 17:01:49 +0800

    Add the Airbnb list exercise commend.

--- Forms/Models/SearchGroup.cs
+++ Forms/Models/SearchGroup.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace Forms.Models
 {
+    // I've derived SearchGroup from ObservableCollection instead of List, because
+    // this is the object that we'll be bind the ListView to. So, when we remove
+    // a Search from SearchGroup, we want the ListView to be notified. 
     public class SearchGroup : ObservableCollection<Search>
     {
         public string Title { get; set; }
--- Forms/Services/SearchService.cs
+++ Forms/Services/SearchService.cs
@@ -6,8 +6,23 @@ using System.Text;
 
 namespace Forms.Services
 {
+    // User's searches can be stored either in a database on the device, or 
+    // somewhere in the cloud. We use SearchService to work with the persistence
+    // store for the app. So this class hides the complexity behind accessing
+    // the recent searches. Whether they're stored locally or in the cloud, it 
+    // doesn't matter from the perspective of the user interface.
+
+    // Note nowhere in this class we're using an ObservableCollection, because 
+    // this is a collection type that we often use in Xamarin or WPF apps. 
+    // In theory, we should be able to re-use this SearchService in an ASP.NET MVC 
+    // web app. We don't have ObservableCollection in web apps. So, this class
+    // should not know anything about ObservableCollection. GetRecentSearches method simply returns an
+    // IEnumerable. 
+
     public class SearchService
     {
+        // Currently I've hardcoded the list of searches here. But they could be
+        // stored in a database or fetched via a remote service. 
         private List<Search> _searches = new List<Search>
         {
             new Search
@@ -32,6 +47,8 @@ namespace Forms.Services
             {
                 return _searches;
             }
+            // Note that I've used StringComparison.CurrentCultureIgnoreCase 
+            // so searching is case-insensitive.
             return _searches.Where(s => s.Location.StartsWith(filter, StringComparison.CurrentCultureIgnoreCase));
         }
         public void DeleteSearch(int searchId)
