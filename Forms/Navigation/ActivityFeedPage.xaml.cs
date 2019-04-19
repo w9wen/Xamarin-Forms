599a415ceb6fcda0885bebe0bf21d1d360211782
William Wen (NB) <william.wen@tac.com.tw>
2019-04-20 04:40:12 +0800

    6-68. The activity feed page.

---
+++ Forms/Navigation/ActivityFeedPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Forms.Models;
+using Forms.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms.Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActivityFeedPage : ContentPage
+    {
+        private ActivityService _activityService = new ActivityService();
+        public ActivityFeedPage()
+        {
+            InitializeComponent();
+            ListView_ActivityFeed.ItemsSource = _activityService.GetActivities();
+        }
+
+        private void ListView_ActivityFeed_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var activity = e.SelectedItem as Activity;
+            ListView_ActivityFeed.SelectedItem = null;
+        }
+    }
+}
