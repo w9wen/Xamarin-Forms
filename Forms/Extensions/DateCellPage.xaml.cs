951c3edf68bfebf2ed92a5a5a7aca7f6d3b06db0
William Wen (NB) <william.wen@tac.com.tw>
2019-04-23 16:53:09 +0800

    7-78. Bindable Properties.

---
+++ Forms/Extensions/DateCellPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms.Extensions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateCellPage : ViewCell
+    {
+        public static readonly BindableProperty LabelTextProperty=
+            BindableProperty.Create("LabelText", typeof(string),typeof(DateCellPage));
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+        public DateCellPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
